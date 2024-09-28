d3984b8adc36b3442f4b452241492c3677a250cc
cjay <scottishman666@gmail.com>
2024-09-28 14:29:55 +0800

    Made a test bow

---
+++ Content/Weapons/testbow.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+using Terraria.ModLoader;
+
+namespace WeaponExpansion.Content.Weapons
+{
+    internal class testbow : ModItem 
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 24;
+        }
+
+        public override void AddRecipes()
+        {
+
+        }
+    }
+}
