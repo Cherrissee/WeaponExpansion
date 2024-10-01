d76893ebf124768cca5443be33463ca4f16f6657
cjay <scottishman666@gmail.com>
2024-10-01 09:27:21 +0800

    Update the sprites for Annihilator and we have an Icon finally !!

---
+++ Content/Weapons/Timebow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace WeaponExpansion.Content.Weapons
+{
+    internal class Timebow : ModItem
+    {
+        public override void SetDefaults()
+        {
+
+        }
+    }
+}
